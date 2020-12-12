dd6d0c008672ea426e1a1b8764fbea91ef1503d2
will0212 <test.seungho@gmail.com>
2020-12-13 00:34:52 +0900

    fix(upload): 업로드 기능 에러 수정

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,10 +1,11 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
+require("dotenv").config();
 
 const s3 = new AWS.S3({
-  accessKey: process.env.accessKey,
-  secretKey: process.env.secretKey,
+  accessKeyId: process.env.accessKey,
+  secretAccessKey: process.env.secretKey,
   region: process.env.REGION,
 });
 
@@ -13,10 +14,7 @@ const storage = multerS3({
   bucket: "loltogether-bucket",
   contentType: multerS3.AUTO_CONTENT_TYPE,
   acl: "public-read",
-  metadata: function (req, file, cb) {
-    cb(null, { filedName: file.filedname });
-  },
-  key: function (req, file, cb) {
+  key: (req, file, cb) => {
     cb(null, `uploads/${Date.now()}_${file.originalname}`);
   },
 });
