d113f21b417dcca12f44597724ad020087050c44
yerimkoko <gochi97@naver.com>
2021-06-09 14:42:47 +0900

    fix: db 설정변경

--- sql/V1__init.sql
+++ sql/V1__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE comment
+CREATE TABLE IF NOT EXISTS  comment
 (
     `id`                 BIGINT          NOT NULL    AUTO_INCREMENT, 
     `board_id`           BIGINT          NOT NULL, 
@@ -9,7 +9,7 @@ CREATE TABLE comment
 );
 
 
-CREATE TABLE member
+CREATE TABLE IF NOT EXISTS  member
 (
     `id`                 BIGINT          NOT NULL    AUTO_INCREMENT, 
     `email`              VARCHAR(100)    NOT NULL, 
@@ -22,11 +22,11 @@ CREATE TABLE member
 );
 
 
-CREATE TABLE board
+CREATE TABLE IF NOT EXISTS  board
 (
     `id`                 BIGINT         NOT NULL    AUTO_INCREMENT, 
     `member_id`          BIGINT         NULL,
-    'line'               VARCHAR(30)    NULL, 
+    `line`               VARCHAR(30)    NULL, 
     `title`              VARCHAR(50)    NULL, 
     `content`            TEXT           NULL, 
     `created_data_time`  DATETIME       NULL, 
